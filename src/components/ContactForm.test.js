5a6a0af15e88fd3b3b3ad5727d4772cf4f731e12
Matthew Bolton <matthewwbolton@gmail.com>
2020-03-12 15:17:46 -0400

    setup components, installed dependencies and setup testing environment

---
+++ src/components/ContactForm.test.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { render, fireEvent } from "@testing-library/react";
+import ContactForm from './ContactForm';
+
+test('form renders correctly', () => {
+   const { } = render(<ContactForm />);
+});
+
